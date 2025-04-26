f94af12b699db00d88e7d5ea8060b9cf66a5fa3f
Abhinav Prakash <abprakash85409@gmail.com>
2025-04-26 18:15:27 +0530

    feat: add endpoint to to fetch incident by ID

--- src/controllers/incident.controller.js
+++ src/controllers/incident.controller.js
@@ -1,13 +1,13 @@
-import Incident from "../models/incident.model.js";
-import appResponse from "../utils/appResponse.js";
-import { AppError } from "../utils/appError.js";
-import validateIncident from "../validations/incident.validate.js";
+import Incident from '../models/incident.model.js';
+import appResponse from '../utils/appResponse.js';
+import { AppError } from '../utils/appError.js';
+import validateIncident from '../validations/incident.validate.js';
 export const getIncidents = async (req, res, next) => {
   try {
     const incidents = await Incident.find();
     appResponse(res, {
       data: incidents,
-      message: "Incidents fetched successfully",
+      message: 'Incidents fetched successfully',
     });
   } catch (error) {
     next(error);
@@ -22,7 +22,7 @@ export const createIncident = async (req, res, next) => {
     console.log(result.length);
     if (result.length > 0) {
       throw new AppError({
-        message: "Validation failed",
+        message: 'Validation failed',
         statusCode: 400,
         errors: result,
       });
@@ -31,7 +31,7 @@ export const createIncident = async (req, res, next) => {
     await newIncident.save();
     appResponse(res, {
       data: newIncident,
-      message: "Incident created successfully",
+      message: 'Incident created successfully',
       statusCode: 201,
     });
   } catch (error) {
@@ -39,4 +39,15 @@ export const createIncident = async (req, res, next) => {
   }
 };
 
+export const getIncidentById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const found = await Incident.find({ _id: id });
+    console.log(found);
+    if (!found) throw new AppError({ message: 'incident not found', statusCode:404 });
+    appResponse(res, {message : "Incident found", data : found})
+  } catch (err) {
+    next(err)
+  }
+};
 
--- src/routes/incident.route.js
+++ src/routes/incident.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import {createIncident, getIncidents,} from "../controllers/incident.controller.js";
+import {createIncident, getIncidentById, getIncidents,} from "../controllers/incident.controller.js";
  
 const router = Router();
 
 router.route('/incidents')
 .get(getIncidents)
 .post(createIncident)
+
+router.route('/incidents/:id')
+.get(getIncidentById)
 
 export default router;
